67afe28d550ff383d8ab0a66d4d63654dcebd8f6
Jennifer Nugent <jmn2158@columbia.edu>
2020-06-05 15:29:27 -0400

    g

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  entry: './client/App.js', // assumes your entry point is the index.js in the root of your project folder
+  entry: './server/server.js', // assumes your entry point is the index.js in the root of your project folder
   mode: 'development',
   output: {
     path: __dirname, // assumes your bundle.js will also be in the root of your project folder
